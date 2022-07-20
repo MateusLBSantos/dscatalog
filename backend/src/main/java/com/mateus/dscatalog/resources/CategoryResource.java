62636e599805615748af872b6f893b8fb0835333
MateusLBSantos <mateuslucas.ita@hotmail.com>
2022-07-20 10:29:20 -0300

    Update Category

--- backend/src/main/java/com/mateus/dscatalog/resources/CategoryResource.java
+++ backend/src/main/java/com/mateus/dscatalog/resources/CategoryResource.java
@@ -46,7 +46,7 @@ public class CategoryResource {
 		return ResponseEntity.created(uri).body(dto);
 	}
 	
-	@PutMapping(value = "{/id}")
+	@PutMapping(value = "/{id}")
 	public ResponseEntity<CategoryDTO> update(@PathVariable Long id, @RequestBody CategoryDTO dto) {
 		dto = service.update(id, dto);
 		
